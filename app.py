4e6cc0c2701317e379e5a0a8988064697f1083a6
Akshat Saxena <akshatsaxena890@gmail.com>
2024-09-14 07:58:22 +0530

    Second Commit

--- app.py
+++ app.py
@@ -8,6 +8,15 @@ def homepage():
     return render_template("index.html")
 
 
+@app.route("/products")
+def products():
+    return render_template("products.html")
+
+
+@app.route("/productsdetails")
+def product_details():
+    return render_template("productsdetails.html")
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0" , port=8888 , debug=True)
